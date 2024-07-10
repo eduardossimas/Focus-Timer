7090774432599182b2736af3d356c749a007d35a
eduardossimas <dudusalzer13@gmail.com>
2024-07-10 13:14:15 -0300

    Mudando o som de chuva

--- src/SoundsBg/sounds.js
+++ src/SoundsBg/sounds.js
@@ -1,10 +1,10 @@
-export const musicForest = new Audio('https://raw.githubusercontent.com/eduardossimas/Focus-Timer/master/assets/Floresta.wav');
+export const musicForest = new Audio('../assets/Floresta.wav');
 
-export const musicRain = new Audio('https://raw.githubusercontent.com/eduardossimas/Focus-Timer/master/assets/Chuva.wav');
+export const musicRain = new Audio('../assets/rain-sounds-the-sound-of-summer-rain-141793.mp3');
 
-export const musicCoffee = new Audio('https://raw.githubusercontent.com/eduardossimas/Focus-Timer/master/assets/Cafeteria.wav');
+export const musicCoffee = new Audio('../assets/Cafeteria.wav');
 
-export const musicFire = new Audio('https://raw.githubusercontent.com/eduardossimas/Focus-Timer/master/assets/Lareira.wav');
+export const musicFire = new Audio('../assets/Lareira.wav');
 
 musicForest.loop = true;
 musicRain.loop = true;
